5d9309657d3f6eb9f98558c066116ee87e1f64c4
Brian H Wilson <bwilson@co.clatsop.or.us>
2022-09-27 16:08:54 -0700

    using zip instead of 7z

--- make_zip.sh
+++ make_zip.sh
@@ -4,6 +4,6 @@ FOLDER=ORMAPToolsV3.0
 ARCHIVE=$FOLDER.zip
 
 rm -f $ARCHIVE
-test -n -d $FOLDER && mkdir $FOLDER
+mkdir $FOLDER
 for item in `cat manifest.txt`; do cp $item ${FOLDER}/; done
 zip -r $ARCHIVE ${FOLDER}
