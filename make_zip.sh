d02ffe26ddcf062ed1fffeef4e60f6be9bb378e2
Brian H Wilson <bwilson@co.clatsop.or.us>
2022-09-28 10:15:54 -0700

    Put python in its own folder, ORMAPTools, and changed make_zip and manifest to match. Updated toolbox.

--- make_zip.sh
+++ make_zip.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-FOLDER=ORMAPToolsV3.0
+FOLDER=ORMAPToolsV3.0.1
 ARCHIVE=$FOLDER.zip
 
 rm -f $ARCHIVE
 mkdir $FOLDER
-for item in `cat manifest.txt`; do cp $item ${FOLDER}/; done
+for item in `cat manifest.txt`; do cp -r $item ${FOLDER}/; done
 zip -r $ARCHIVE ${FOLDER}
